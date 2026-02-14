ca4429ffa62d035e78f43160e819b6ffee7906ad
grimlink <sean.grimlink@gmail.com>
2026-02-14 14:18:13 +0100

    IMP: small improvments

--- view/frontend/tailwind/tailwind.config.js
+++ view/frontend/tailwind/tailwind.config.js
@@ -1,3 +1,9 @@
+/**
+ * @author Siteation (https://siteation.dev/)
+ * @copyright Copyright 2021 Siteation (https://siteation.dev/)
+ * @license MIT
+ */
+
 module.exports = {
     purge: {
         content: ["../templates/**/*.phtml", "../layout/**/*.xml"],
